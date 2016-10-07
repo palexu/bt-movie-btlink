400a2bb293604f85bb74b247db2542e9920e94e9
xujunyu <1098672878@qq.com>
2016-10-07 21:54:34 +0800

    现在可以从命令行输入电影名称了     bug:搜索电影时，只获取了一部分电影

--- Spider.py
+++ Spider.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import sys
 class Spider:
     headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0",
         "Accept-Language":"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -10,25 +11,16 @@ class Spider:
         }
 
     def search(self,movie):
-        param={'keyword':self.toGb2312(movie)}
+        param={'keyword':Transfer.toGb2312(movie)}
         url='http://so.loldytt.com/search.asp'
         html=requests.post(url,params=param)
         # html=requests.post("http://so.loldytt.com/search.asp?keyword=%B0%A2%B8%CA")
         bsobj=BeautifulSoup(html.text)
-        htmlContent=self.toUtf8(html.text)
-        urldic=self.matchUrl(htmlContent)
-        for i in urldic:
-            if i['name']==movie:
-                return i
+        htmlContent=Transfer.toUtf8(html.text)
+        urldic=self.__matchUrl(htmlContent)
         return urldic
 
-    def toGb2312(self,text):
-        return text.encode('gb2312')
-
-    def toUtf8(self,text):
-        return text.encode('ISO-8859-1').decode('gb2312')
-
-    def matchMagnet(self,text):
+    def __matchMagnet(self,text):
         pattern=re.compile(r'thunder:\/\/[A-Za-z0-9\+\/=]*')
         match=pattern.findall(text)
         print(match)
@@ -39,17 +31,19 @@ class Spider:
 
     def getMagnet(self,url):
         html=requests.get(url)
-        bsobj=BeautifulSoup(self.toUtf8(html.text))
+        bsobj=BeautifulSoup(Transfer.toUtf8(html.text))
         lst=[]
         for i in bsobj.find_all('a',{'target':'_self'}):
             print(i.get_text())
-            rs=self.matchMagnet(str(i))
+            rs=self.__matchMagnet(str(i))
             if rs!='':
                 lst.append({'name':i.get_text(),'thunderLink':rs})
-        print(lst)
+        for i in lst:
+            if '1024' not in i['name']:
+                print(i['thunderLink'])
         return lst
 
-    def matchUrl(self,html):
+    def __matchUrl(self,html):
         pattern=re.compile(r'http://www\.loldytt\.com/.*/.*/">.+</a>')
         urlPattern=re.compile(r'http://www\.loldytt\.com/.*/.*/')
         match=pattern.findall(html)
@@ -61,17 +55,38 @@ class Spider:
             lst.append({'name':mvName,'url':url})
         return lst
 
+    def found(self,movie,inputList):
+        for i in inputList:
+            if movie==i['name']:
+                return i['url']
+        return False
+
 class test_spider:
     def test_matchMagnet(self,text):
         sp=Spider()
         tt='<a href="thunder://QUFmdHA6Ly9keTpkeUB4bGEueHVuYm8uY2M6MTAzNjgvW9G4wNfPwtTYd3d3Llh1bkJvLkNjXbCiuMrV/bSrQkQxMDI0uN/H5dbQ06LLq9fWLnJtdmJaWg==" title="阿甘正传BD1024高清中英双字" target="_self">阿甘正传BD1024高清中英双字</a>'
         print(spider.matchMagnet(tt))
 
+class Transfer:
+    def toGb2312(text):
+        return text.encode('gb2312')
+
+    def toUtf8(text):
+        return text.encode('ISO-8859-1').decode('gb2312')
+
 if __name__ == '__main__':
     spider=Spider()
-    rs=spider.search('美国恐怖故事第二季')
-    print(rs)
-    spider.getMagnet(rs['url'])
+    if len(sys.argv)>=2:
+        movie=sys.argv[1]
+        rs=spider.search(movie)
+        hasFound=spider.found(movie,rs)
+        if hasFound:
+            spider.getMagnet(hasFound)
+        else:
+            for i in rs:
+                print(i['name'])
+
+        # spider.getMagnet(rs['url'])
     # bthtml=requests.get(rs['url'])
     # print(spider.toUtf8(bthtml.text))
     # print(spider.matchMagnet(bthtml.text))
