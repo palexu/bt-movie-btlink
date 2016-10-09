c222f02b06b696b05457c7027c4fb701059021c5
xujunyu <1098672878@qq.com>
2016-10-09 22:04:31 +0800

    imp

--- Spider.py
+++ Spider.py
@@ -1,6 +1,7 @@
 #coding=utf-8
-import requests
 from bs4 import BeautifulSoup
+import requests
+import urllib.parse
 import re
 import sys
 
@@ -18,8 +19,25 @@ class Spider:
         html=requests.post(url,params=param)
         bsobj=BeautifulSoup(html.text, "html.parser")
         htmlContent=Transfer.toUtf8(html.text)
-        urldic=self.__matchUrl(htmlContent)
-        return urldic
+
+        movieCount=self.__getNumOfMovie(htmlContent)
+        print('为你找到了%d部电影～回复序号获得迅雷链接'%movieCount)
+        urllist=[]
+        n=movieCount//20
+        m=movieCount%20
+        if m>0:
+            n=n+1
+        for i in range(1,n+1):
+            param={'keyword':Transfer.toGb2312(movie),'page':i}
+            html=requests.post(url,params=param)
+            bsobj=BeautifulSoup(html.text, "html.parser")
+            htmlContent=Transfer.toUtf8(html.text)
+            urllist.append(self.__matchUrl(htmlContent))
+        rs=[]
+        for i in urllist:
+            for j in i:
+                rs.append(j)
+        return rs
 
     def __matchMagnet(self,text):
         pattern=re.compile(r'thunder:\/\/[A-Za-z0-9\+\/=]*')
@@ -41,8 +59,9 @@ class Spider:
 
     def __matchUrl(self,html):
         pattern=re.compile(r'http://www\.loldytt\.com/.*/.*/">.+</a>')
-        urlPattern=re.compile(r'http://www\.loldytt\.com/.*/.*/')
+        urlPattern=re.compile(r'http://www\.loldytt\.com/[\w|\d]+/[\w|\d]+/')
         match=pattern.findall(html)
+
         lst=[]
         for i in match:
             tmpstr=i.replace('</a>','')
@@ -50,6 +69,13 @@ class Spider:
             mvName=tmpstr.replace(url+'">','')
             lst.append({'name':mvName,'url':url})
         return lst
+
+    def __getNumOfMovie(self,html):
+        pattern=re.compile(r'共找到<b>\d+</b>部')
+        match=pattern.findall(html)
+        if match:
+            num=match[0].replace('共找到<b>','').replace('</b>部','')
+            return int(num)
 
     def found(self,movie,inputList):
         for i in inputList:
@@ -59,32 +85,38 @@ class Spider:
 
 
 class Movie:
-    spider=Spider()
-
+    
     def getMovie(self,movie):
+        spider=Spider()
         rs=spider.search(movie)
         hasFound=spider.found(movie,rs)
-        if hasFound:
-            result=spider.getMagnet(hasFound)
-            for item in result:
-                print("%s  %s"%(item['name'],item['thunderLink']))
-        else:
-            for i in rs:
-                print(i['name'])
+        # if hasFound:
+        #     result=spider.getMagnet(hasFound)
+        #     for item in result:
+        #         print("%s  %s"%(item['name'],item['thunderLink']))
+        # else:
+        #     for i in rs:
+        #         print(i['name'])
+        for index,item in enumerate(rs):
+            print('%d.%s'%(index,item['name']))
 
 
 class test:
     def printMovieInfo(self):
-        print("""美国恐怖故事第二季EP01  thunder://QUFmdHA6Ly90djp0dkB4bGguMnR1LmNjOjMxNDU2L8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDAxLnJtdmJaWg==
-美国恐怖故事第二季EP02  thunder://QUFmdHA6Ly9kczpkc0B4bGMuMnR1LmNjOjIxMjkyL8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDAyLnJtdmJaWg==
-美国恐怖故事第二季EP03  thunder://QUFmdHA6Ly9kczpkc0B4bGMuMnR1LmNjOjIxMjk2L8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDAzLnJtdmJaWg==
-美国恐怖故事第二季EP04  thunder://QUFmdHA6Ly9kczpkc0B4bGMuMnR1LmNjOjMxMjE3L8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDA0LnJtdmJaWg==
+        print("""美国恐怖故事第二季EP01  thunder://QUFmdHA6Ly90djp0dkB4bGguMnR1LmNjOjMxNDU2L8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDAxLnJtdmJaWg==\n
+美国恐怖故事第二季EP02  thunder://QUFmdHA6Ly9kczpkc0B4bGMuMnR1LmNjOjIxMjkyL8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDAyLnJtdmJaWg==\n
+美国恐怖故事第二季EP03  thunder://QUFmdHA6Ly9kczpkc0B4bGMuMnR1LmNjOjIxMjk2L8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDAzLnJtdmJaWg==\n
+美国恐怖故事第二季EP04  thunder://QUFmdHA6Ly9kczpkc0B4bGMuMnR1LmNjOjMxMjE3L8PAufq/1rLAucrKwrXatv68vi9b0bjA18/C1Nh3d3cuMnR1LmNjXcPAufq/1rLAucrKwi612rb+vL5FUDA0LnJtdmJaWg==\n
 """)
 
-    def test_matchMagnet(self,text):
+    def test_matchMagnet(self):
         sp=Spider()
         tt='<a href="thunder://QUFmdHA6Ly9keTpkeUB4bGEueHVuYm8uY2M6MTAzNjgvW9G4wNfPwtTYd3d3Llh1bkJvLkNjXbCiuMrV/bSrQkQxMDI0uN/H5dbQ06LLq9fWLnJtdmJaWg==" title="阿甘正传BD1024高清中英双字" target="_self">阿甘正传BD1024高清中英双字</a>'
         print(spider.matchMagnet(tt))
+
+    def test_isUrlCode(self):
+        testString='E6%B5%8B%E你8%AF%95'
+        print(isUrlCode(testString))
 
 
 class Transfer:
@@ -94,6 +126,13 @@ class Transfer:
     def toUtf8(text):
         return text.encode('ISO-8859-1').decode('gb2312')
 
+def isUrlCode(code):
+    #有问题
+    match=re.match(r'^[%\w]+$',code)
+    if match:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     if len(sys.argv)>=2:
@@ -101,7 +140,9 @@ if __name__ == '__main__':
         if movie=='test':
             te=test()
             te.printMovieInfo()
+            # te.test_isUrlCode()
         else:
+            movie=urllib.parse.unquote(movie)
             movieHandler=Movie()
             movieHandler.getMovie(movie)
 
